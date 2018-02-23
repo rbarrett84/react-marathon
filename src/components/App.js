029cea95e8eeb5c6525c360f15539f47d5a04ca7
rbarrett84 <rbarrett84@yahoo.com>
2018-02-22 19:46:23 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let data = this.props.data
+
+    let selectedPlaylistSongIds = data.playlists[this.props.data.selectedPlaylistId-1].songs;
+
+    let filterById = (obj) => {
+      return selectedPlaylistSongIds.includes(obj.id);
+    }
+
+    let selectedPlaylistSongs = data.songs.filter(filterById);
+
+    return (
+      <div className="App row">
+        <h1>This is the Playlist Portion of our App</h1>
+        <PlayListCollection
+          playlist={data.playlist}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
