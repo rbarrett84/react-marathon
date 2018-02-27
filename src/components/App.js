3ed835f9b2874379abebb5a493d6f7a7ba57422b
rbarrett84 <rbarrett84@yahoo.com>
2018-02-27 13:37:30 -0500

    final draft

--- src/components/App.js
+++ src/components/App.js
@@ -17,25 +17,23 @@ class App extends React.Component {
     let selectedPlaylistSongs = data.songs.filter(filterById);
 
     return (
+      <div>
       <div className="App row">
-        <div className="small-6 columns">
-          <h1>This is the Playlist Portion of our App</h1>
-          <PlaylistCollection
-            playlists={data.playlists}
-            selectedPlaylistId={this.state.selectedPlaylistId}
-            handlePlaylistSelect={this.handlePlaylistSelect}
-          />
-        </div>
-        <div className="small-6 columns">
-          <h1>This is the song list portion of our App!</h1>
-          <SongCollection
-            songs={selectedPlaylistSongs}
-            selectedSongId={this.state.selectedSongId}
-            handleSongSelect={this.handleSongSelect}
-
-          />
-        </div>
+        <h1>This is the Playlist Portion of our App</h1>
+        <PlayListCollection
+          playlist={data.playlist}
+        />
       </div>
+
+      <div className="small-6 columns">
+        <h1>This is the song list portion of our App!</h1>
+        <SongCollection
+          songs={selectedPlaylistSongs}
+          selectedSongId={this.state.selectedSongId}
+          handleSongSelect={this.handleSongSelect}
+        />
+      </div>
+    </div>
     );
   }
 }
